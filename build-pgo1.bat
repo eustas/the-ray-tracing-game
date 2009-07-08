af83d3ba8e1a3faff792e1c2d36dd7cc2db3f2a2
eustas.ru <eustas.ru@0945feb0-6b02-11de-ba0c-230037762ff9>
2009-07-08 03:48:56 +0000

    different executables now have different names

--- build-pgo1.bat
+++ build-pgo1.bat
@@ -1,10 +1,19 @@
 @call env.bat
 @rem /Wall /Wcheck /Wport /FAs /O3
 
+del  src\render-by-lines.class.Aa.cpp
+copy src\render-by-lines.class.cpp src\render-by-lines.class.Aa.cpp
+del  src\render-by-lines.class.Gp.cpp
+copy src\render-by-lines.class.cpp src\render-by-lines.class.Gp.cpp
+del  src\render-by-lines.class.AaGp.cpp
+copy src\render-by-lines.class.cpp src\render-by-lines.class.AaGp.cpp
+
+del *.dyn
+
 set librs=tbb-static.lib User32.Lib Kernel32.Lib Gdi32.Lib Winmm.lib
 set linkopt=ray.res /NODEFAULTLIB:tbb.lib /MACHINE:X86
 set defins=/DUSE_WINTHREAD /D__TBB_TASK_CPP_DIRECTLY_INCLUDED=1 /D_WIN32 /DUNICODE
 @rem /O3
-icl /GX /fast /Qprof-gen %defins% /nologo /MT /Fo.\ /Fe.\distr\ray.exe .\src\main.cpp %librs% /link %linkopt% >build.log
+icl /GX /fast /Qprof-gen %defins% /nologo /MT /Fo.\ /Fe.\distr\ray.dyn.exe .\src\main.cpp %librs% /link %linkopt% >build-pgo1.log
 
 @rem copy build.log con
