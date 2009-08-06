82df61e3cf315bc365172d65d4d33ecb7e35e5aa
eustas.ru <eustas.ru@0945feb0-6b02-11de-ba0c-230037762ff9>
2009-08-06 19:30:35 +0000

    stub-copy en resources

--- trunk/build.bat
+++ trunk/build.bat
@@ -12,6 +12,7 @@ set librs=tbb-static.lib User32.Lib Kernel32.Lib Gdi32.Lib Winmm.lib png.lib
 set linkopt=ray.res /NODEFAULTLIB:tbb.lib /MACHINE:X86
 set defins=/DUSE_WINTHREAD /D__TBB_TASK_CPP_DIRECTLY_INCLUDED=1 /D_WIN32 /DUNICODE
 @rem /O3 /P /Qipo /fast  /Qvec- /Qprec-div- /Qprec-sqrt- /Qfast-transcendentals /Qvec-
-icl /GX %defins% /fast /nologo /MT /Fo.\ /Fe.\distr\ray.exe .\src\main.cpp %librs% /link %linkopt% >build.log
+icl /GX %defins% /fast /nologo /MT /Fo.\ /Fe.\distr-ru\ray.exe .\src\main.cpp %librs% /link %linkopt% >build.log
+copy distr-ru\ray.exe distr-en
 
 @rem copy build.log con
