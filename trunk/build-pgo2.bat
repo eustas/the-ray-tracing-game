ba39cddf13b7d747b5a338078ea09143169a0f00
eustas.ru <eustas.ru@0945feb0-6b02-11de-ba0c-230037762ff9>
2009-08-08 11:53:03 +0000

    refine build procedure

--- trunk/build-pgo2.bat
+++ trunk/build-pgo2.bat
@@ -1,20 +1,7 @@
 @call env.bat
-@rem /Wall /Wcheck /Wport /FAs /O3
-
-del  src\render-by-lines.class.Aa.cpp
-copy src\render-by-lines.class.cpp src\render-by-lines.class.Aa.cpp
-del  src\render-by-lines.class.Gp.cpp
-copy src\render-by-lines.class.cpp src\render-by-lines.class.Gp.cpp
-del  src\render-by-lines.class.AaGp.cpp
-copy src\render-by-lines.class.cpp src\render-by-lines.class.AaGp.cpp
+@call generate-sources.bat
 
 del pgopti.dpi.lock
 del pgopti.dpi
 
-set librs=tbb-static.lib User32.Lib Kernel32.Lib Gdi32.Lib Winmm.lib png.lib
-set linkopt=ray.res /NODEFAULTLIB:tbb.lib /MACHINE:X86
-set defins=/DUSE_WINTHREAD /D__TBB_TASK_CPP_DIRECTLY_INCLUDED=1 /D_WIN32 /DUNICODE
-@rem /O3 /O3 /Qipo
 icl /GX /fast /Qprof-use %defins% /nologo /MT /FAs /Fo.\ /Fe.\distr\ray.pgo.exe .\src\main.cpp %librs% /link %linkopt% >build-pgo2.log
-
-@rem copy build.log con
--- trunk/build.bat
+++ trunk/build.bat
@@ -1,20 +1,4 @@
 @call env.bat
-@rem /Wall /Wcheck /Wport /FAs /O3
+@call generate-sources.bat
 
-del  src\render-by-lines.class.Aa.cpp
-copy src\render-by-lines.class.cpp src\render-by-lines.class.Aa.cpp
-del  src\render-by-lines.class.Gp.cpp
-copy src\render-by-lines.class.cpp src\render-by-lines.class.Gp.cpp
-del  src\render-by-lines.class.AaGp.cpp
-copy src\render-by-lines.class.cpp src\render-by-lines.class.AaGp.cpp
-
-set librs=tbb-static.lib User32.Lib Kernel32.Lib Gdi32.Lib Winmm.lib png.lib
-set linkopt=ray.res /NODEFAULTLIB:tbb.lib /MACHINE:X86
-set defins=/DUSE_WINTHREAD /D__TBB_TASK_CPP_DIRECTLY_INCLUDED=1 /D_WIN32 /DUNICODE
-@rem /O3 /P /Qipo /fast  /Qvec- /Qprec-div- /Qprec-sqrt- /Qfast-transcendentals /Qvec-
 icl /GX %defins% /fast /nologo /MT /Fo.\ /Fe.\distr-ru\ray.exe .\src\main.cpp %librs% /link %linkopt% >build.log
-copy distr-ru\ray.exe distr-en
-copy distr\*.* distr-ru
-copy distr\*.* distr-en
-
-@rem copy build.log con
