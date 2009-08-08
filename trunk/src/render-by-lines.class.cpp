398f6d786bfbd364888377934f00bb9343441b73
eustas.ru <eustas.ru@0945feb0-6b02-11de-ba0c-230037762ff9>
2009-08-08 11:47:23 +0000

    move parts into class distr ignore patterns

--- trunk/src/render-by-lines.class.cpp
+++ trunk/src/render-by-lines.class.cpp
@@ -1368,13 +1368,479 @@ fullReflect:
 					tResult = LAST_T;
 					doThing = -1;
 					if (state == 0) {
-#include<emi_l_p.cpp>
+//#include<emi_l_p.cpp>
+sx = mx - 0.1 - x0;
+sy = 0.5 - y0;
+sz = (0.5 + mz) - z0;
+k = (sy * vy) + (sz * vz) - (sx * vx * E_INV_16_CONE_H_2);
+coneC = (sy * sy) + (sz * sz) - (sx * sx * E_INV_16_CONE_H_2);
+econeA = (vy * vy) + (vz * vz) - (vx * vx * E_INV_16_CONE_H_2);
+D = k * k - econeA * coneC;
+einvConeA = 1 / econeA;
+if (D <= 0.0) { goto emil1; }
+D = sqrt(D);
+if (econeA > 0.0) {
+	t1 = (k - D) * einvConeA;
+	t2 = (k + D) * einvConeA;
+} else {
+	t2 = (k - D) * einvConeA;
+	t1 = (k + D) * einvConeA;
+}
+x = (mx + 1.0 - 0.1) - (x0 + (vx * t1));
+if (x > E_CONE_H - 0.1) {
+	x = (mx + 1.0 - 0.1) - (x0 + (vx * t2));
+	if (x > E_CONE_H - 0.1) { goto emil1; }
+	t1 = t2;
+}
+if (x < 0.1) { goto emil1; }
+if (t1 < rayFirst) {goto emil1;}
+tmp = E_CONE_YR_P - (E_CONE_YR_Y * x); // tmp == r
+if (tmp <= 0.0) {goto emil1;}
+tmp = 1 / tmp;
+cot = t1;
+t2 = t1 - 0.00001;
+x = x0 + vx * t2;
+y = y0 + vy * t2;
+z = z0 + vz * t2;
+nx = -E_CONE_DY;
+ny =  (y - 0.5) * tmp;
+nz = (z - mz - 0.5) * tmp;
+tResult = t1; doThing = 0; cox = x; coy = y; coz = z; conx= nx; cony = ny; conz = nz;
+emil1:
+if (vx == 0.0) {goto emil2;}
+t1 = (mx - x0) / vx;
+if (t1 < rayFirst) {goto emil2;}
+if (t1 > tResult) {goto emil2;}
+y = y0 + vy * t1 - 0.5;
+z = z0 + vz * t1 - mz - 0.5;
+t2 = (y * y) + (z * z);
+if (t2 > RAY_R_2) {goto emil2;}
+cot = t1;
+tResult = t1; doThing = 1;
+emil2:
+a = (vx * vx * 16.0) + (vy * vy) + (vz * vz);
+x = mx + 0.25 - x0;
+y = 0.5 - y0;
+z = mz + 0.5 - z0;
+k = (16.0 * x * vx) + (y * vy) + (z * vz);
+c = (16.0 * x * x) + (y * y) + (z * z) - 0.04;
+D = k * k - a * c;
+if (D < 0.0) {goto emil3;}
+D = sqrt(D);
+a = 1 / a;
+t1 = (k - D) * a;
+if (t1 < rayFirst) {t1 = (k + D) * a;}
+if (t1 < rayFirst) {goto emil3;}
+if (t1 > tResult) {goto emil3;}
+t1 = t1 - 0.0001;
+cox = x0 + vx * t1;
+coy = y0 + vy * t1;
+coz = z0 + vz * t1;
+conx = 5.0 * 4.0 * (cox - mx - 0.25);
+cony = 5.0 * (coy - 0.5);
+conz = 5.0 * (coz - 0.5 - mz);
+cot = t1;
+tResult = t1; doThing = 2;
+emil3:
+a = (vx * vx * 1.44) + (vy * vy) + (vz * vz);
+x = mx + 0.9 - x0;
+y = 0.5 - y0;
+z = mz + 0.5 - z0;
+k = (1.44 * x * vx) + (y * vy) + (z * vz);
+c = (1.44 * x * x) + (y * y) + (z * z) - 0.09;
+D = k * k - a * c;
+if (D < 0.0) {goto emil4;}
+D = sqrt(D);
+a = 1 / a;
+t1 = (k - D) * a;
+if (t1 < rayFirst) {t1 = (k + D) * a;}
+if (t1 < rayFirst) {goto emil4;}
+if (t1 > tResult) {goto emil4;}
+t1 = t1 - 0.0001;
+cox = x0 + vx * t1;
+if (cox > (mx + 1.0)) { goto emil4;}
+coy = y0 + vy * t1;
+coz = z0 + vz * t1;
+conx = 3.333333333333333333 * 1.2 * (cox - mx - 0.9);
+cony = 3.333333333333333333 * (coy - 0.5);
+conz = 3.333333333333333333 * (coz - 0.5 - mz);
+cot = t1;
+tResult = t1; doThing = 3;
+emil4:
+if (vx == 0.0) {goto emi5;}
+t1 = (mx + 1.0 - x0) / vx;
+if (t1 < rayFirst) {goto emi5;}
+if (t1 > tResult) {goto emi5;}
+y = y0 + vy * t1 - 0.5;
+z = z0 + vz * t1 - mz - 0.5;
+t2 = (y * y) + (z * z);
+if (t2 > CUT_EMI_R_2) {goto emi5;}
+t1 = t1 - 0.0001;
+cox = x0 + vx * t1;
+coy = y0 + vy * t1;
+coz = z0 + vz * t1;
+conx = 1.0;
+cony = 0.0;
+conz = 0.0;
+cot = t1;
+tResult = t1; doThing = 4;
+
 					} else if (state == 1) {
-#include<emi_u_p.cpp>
+//#include<emi_u_p.cpp>
+sz = mz - 0.1 - z0;
+sy = 0.5 - y0;
+sx = (0.5 + mx) - x0;
+k = (sy * vy) + (sx * vx) - (sz * vz * E_INV_16_CONE_H_2);
+coneC = (sy * sy) + (sx * sx) - (sz * sz * E_INV_16_CONE_H_2);
+econeA = (vy * vy) + (vx * vx) - (vz * vz * E_INV_16_CONE_H_2);
+D = k * k - econeA * coneC;
+einvConeA = 1 / econeA;
+if (D <= 0.0) { goto emiu1; }
+D = sqrt(D);
+if (econeA > 0.0) {
+	t1 = (k - D) * einvConeA;
+	t2 = (k + D) * einvConeA;
+} else {
+	t2 = (k - D) * einvConeA;
+	t1 = (k + D) * einvConeA;
+}
+z = (mz + 1.0 - 0.1) - (z0 + (vz * t1));
+if (z > E_CONE_H - 0.1) {
+	z = (mz + 1.0 - 0.1) - (z0 + (vz * t2));
+	if (z > E_CONE_H - 0.1) { goto emiu1; }
+	t1 = t2;
+}
+if (z < 0.1) { goto emiu1; }
+if (t1 < rayFirst) {goto emiu1;}
+tmp = E_CONE_YR_P - (E_CONE_YR_Y * z); // tmp == r
+if (tmp <= 0.0) {goto emiu1;}
+tmp = 1 / tmp;
+cot = t1;
+t2 = t1 - 0.00001;
+z = z0 + vz * t2;
+y = y0 + vy * t2;
+x = x0 + vx * t2;
+nz = -E_CONE_DY;
+ny =  (y - 0.5) * tmp;
+nx = (x - mx - 0.5) * tmp;
+tResult = t1; doThing = 0; cox = x; coy = y; coz = z; conx= nx; cony = ny; conz = nz;
+emiu1:
+if (vz == 0.0) {goto emiu2;}
+t1 = (mz - z0) / vz;
+if (t1 < rayFirst) {goto emiu2;}
+if (t1 > tResult) {goto emiu2;}
+y = y0 + vy * t1 - 0.5;
+x = x0 + vx * t1 - mx - 0.5;
+t2 = (y * y) + (x * x);
+if (t2 > RAY_R_2) {goto emiu2;}
+cot = t1;
+tResult = t1; doThing = 1;
+emiu2:
+a = (vz * vz * 16.0) + (vy * vy) + (vx * vx);
+z = mz + 0.25 - z0;
+y = 0.5 - y0;
+x = mx + 0.5 - x0;
+k = (16.0 * z * vz) + (y * vy) + (x * vx);
+c = (16.0 * z * z) + (y * y) + (x * x) - 0.04;
+D = k * k - a * c;
+if (D < 0.0) {goto emiu3;}
+D = sqrt(D);
+a = 1 / a;
+t1 = (k - D) * a;
+if (t1 < rayFirst) {t1 = (k + D) * a;}
+if (t1 < rayFirst) {goto emiu3;}
+if (t1 > tResult) {goto emiu3;}
+t1 = t1 - 0.0001;
+coz = z0 + vz * t1;
+coy = y0 + vy * t1;
+cox = x0 + vx * t1;
+conz = 5.0 * 4.0 * (coz - mz - 0.25);
+cony = 5.0 * (coy - 0.5);
+conx = 5.0 * (cox - 0.5 - mx);
+cot = t1;
+tResult = t1; doThing = 2;
+emiu3:
+a = (vz * vz * 1.44) + (vy * vy) + (vx * vx);
+z = mz + 0.9 - z0;
+y = 0.5 - y0;
+x = mx + 0.5 - x0;
+k = (1.44 * z * vz) + (y * vy) + (x * vx);
+c = (1.44 * z * z) + (y * y) + (x * x) - 0.09;
+D = k * k - a * c;
+if (D < 0.0) {goto emiu4;}
+D = sqrt(D);
+a = 1 / a;
+t1 = (k - D) * a;
+if (t1 < rayFirst) {t1 = (k + D) * a;}
+if (t1 < rayFirst) {goto emiu4;}
+if (t1 > tResult) {goto emiu4;}
+t1 = t1 - 0.0001;
+coz = z0 + vz * t1;
+if (coz > (mz + 1.0)) { goto emiu4;}
+coy = y0 + vy * t1;
+cox = x0 + vx * t1;
+conz = 3.333333333333333333 * 1.2 * (coz - mz - 0.9);
+cony = 3.333333333333333333 * (coy - 0.5);
+conx = 3.333333333333333333 * (cox - 0.5 - mx);
+cot = t1;
+tResult = t1; doThing = 3;
+emiu4:
+if (vz == 0.0) {goto emi5;}
+t1 = (mz + 1.0 - z0) / vz;
+if (t1 < rayFirst) {goto emi5;}
+if (t1 > tResult) {goto emi5;}
+y = y0 + vy * t1 - 0.5;
+x = x0 + vx * t1 - mx - 0.5;
+t2 = (y * y) + (x * x);
+if (t2 > CUT_EMI_R_2) {goto emi5;}
+t1 = t1 - 0.0001;
+coz = z0 + vz * t1;
+coy = y0 + vy * t1;
+cox = x0 + vx * t1;
+conz = 1.0;
+cony = 0.0;
+conx = 0.0;
+cot = t1;
+tResult = t1; doThing = 4;
+
 					} else if (state == 2) {
-#include<emi_r_p.cpp>
+//#include<emi_r_p.cpp>
+sx = mx + 1.1 - x0;
+sy = 0.5 - y0;
+sz = (0.5 + mz) - z0;
+k = (sy * vy) + (sz * vz) - (sx * vx * E_INV_16_CONE_H_2);
+coneC = (sy * sy) + (sz * sz) - (sx * sx * E_INV_16_CONE_H_2);
+econeA = (vy * vy) + (vz * vz) - (vx * vx * E_INV_16_CONE_H_2);
+D = k * k - econeA * coneC;
+einvConeA = 1 / econeA;
+if (D <= 0.0) { goto emir1; }
+D = sqrt(D);
+if (econeA > 0.0) {
+	t1 = (k - D) * einvConeA;
+	t2 = (k + D) * einvConeA;
+} else {
+	t2 = (k - D) * einvConeA;
+	t1 = (k + D) * einvConeA;
+}
+x = (mx + 0.1) - (x0 + (vx * t1));
+if (x < -E_CONE_H + 0.1) {
+	x = (mx + 0.1) - (x0 + (vx * t2));
+	if (x < -E_CONE_H + 0.1) { goto emir1; }
+	t1 = t2;
+}
+x = -x;
+if (x < 0.1) { goto emir1; }
+if (t1 < rayFirst) {goto emir1;}
+tmp = E_CONE_YR_P - (E_CONE_YR_Y * x); // tmp == r
+if (tmp <= 0.0) {goto emir1;}
+tmp = 1 / tmp;
+cot = t1;
+t2 = t1 - 0.00001;
+x = x0 + vx * t2;
+y = y0 + vy * t2;
+z = z0 + vz * t2;
+nx = E_CONE_DY;
+ny =  (y - 0.5) * tmp;
+nz = (z - mz - 0.5) * tmp;
+tResult = t1; doThing = 0; coz = z; coy = y; cox = x; conz= nz; cony = ny; conx = nx;
+emir1:
+if (vx == 0.0) {goto emir2;}
+t1 = (mx + 1.0 - x0) / vx;
+if (t1 < rayFirst) {goto emir2;}
+if (t1 > tResult) {goto emir2;}
+y = y0 + vy * t1 - 0.5;
+z = z0 + vz * t1 - mz - 0.5;
+t2 = (y * y) + (z * z);
+if (t2 > RAY_R_2) {goto emir2;}
+cot = t1;
+tResult = t1; doThing = 1;
+emir2:
+a = (vx * vx * 16.0) + (vy * vy) + (vz * vz);
+x = mx + 0.75 - x0;
+y = 0.5 - y0;
+z = mz + 0.5 - z0;
+k = (16.0 * x * vx) + (y * vy) + (z * vz);
+c = (16.0 * x * x) + (y * y) + (z * z) - 0.04;
+D = k * k - a * c;
+if (D < 0.0) {goto emir3;}
+D = sqrt(D);
+a = 1 / a;
+t1 = (k - D) * a;
+if (t1 < rayFirst) {t1 = (k + D) * a;}
+if (t1 < rayFirst) {goto emir3;}
+if (t1 > tResult) {goto emir3;}
+t1 = t1 - 0.0001;
+cox = x0 + vx * t1;
+coy = y0 + vy * t1;
+coz = z0 + vz * t1;
+conx = 5.0 * 4.0 * (cox - mx - 0.75);
+cony = 5.0 * (coy - 0.5);
+conz = 5.0 * (coz - 0.5 - mz);
+cot = t1;
+tResult = t1; doThing = 2;
+emir3:
+a = (vx * vx * 1.44) + (vy * vy) + (vz * vz);
+x = mx + 0.1 - x0;
+y = 0.5 - y0;
+z = mz + 0.5 - z0;
+k = (1.44 * x * vx) + (y * vy) + (z * vz);
+c = (1.44 * x * x) + (y * y) + (z * z) - 0.09;
+D = k * k - a * c;
+if (D < 0.0) {goto emir4;}
+D = sqrt(D);
+a = 1 / a;
+t1 = (k - D) * a;
+if (t1 < rayFirst) {t1 = (k + D) * a;}
+if (t1 < rayFirst) {goto emir4;}
+if (t1 > tResult) {goto emir4;}
+t1 = t1 - 0.0001;
+cox = x0 + vx * t1;
+if (cox < mx) { goto emir4;}
+coy = y0 + vy * t1;
+coz = z0 + vz * t1;
+conx = 3.333333333333333333 * 1.2 * (cox - mx - 0.1);
+cony = 3.333333333333333333 * (coy - 0.5);
+conz = 3.333333333333333333 * (coz - 0.5 - mz);
+cot = t1;
+tResult = t1; doThing = 3;
+emir4:
+if (vx == 0.0) {goto emi5;}
+t1 = (mx - x0) / vx;
+if (t1 + 0.00002 < rayFirst) {goto emi5;}
+if (t1 > tResult) {goto emi5;}
+y = y0 + vy * t1 - 0.5;
+z = z0 + vz * t1 - mz - 0.5;
+t2 = (y * y) + (z * z);
+if (t2 > CUT_EMI_R_2) {goto emi5;}
+t1 = t1 - 0.0001;
+cox = x0 + vx * t1;
+coy = y0 + vy * t1;
+coz = z0 + vz * t1;
+conx = -1.0;
+cony = 0.0;
+conz = 0.0;
+cot = t1;
+tResult = t1; doThing = 4;
+
 					} else if (state == 3) {
-#include<emi_d_p.cpp>
+//#include<emi_d_p.cpp>
+sz = mz + 1.1 - z0;
+sy = 0.5 - y0;
+sx = (0.5 + mx) - x0;
+k = (sy * vy) + (sx * vx) - (sz * vz * E_INV_16_CONE_H_2);
+coneC = (sy * sy) + (sx * sx) - (sz * sz * E_INV_16_CONE_H_2);
+econeA = (vy * vy) + (vx * vx) - (vz * vz * E_INV_16_CONE_H_2);
+D = k * k - econeA * coneC;
+einvConeA = 1 / econeA;
+if (D <= 0.0) { goto emid1; }
+D = sqrt(D);
+if (econeA > 0.0) {
+	t1 = (k - D) * einvConeA;
+	t2 = (k + D) * einvConeA;
+} else {
+	t2 = (k - D) * einvConeA;
+	t1 = (k + D) * einvConeA;
+}
+z = (mz + 0.1) - (z0 + (vz * t1));
+if (z < -E_CONE_H + 0.1) {
+	z = (mz + 0.1) - (z0 + (vz * t2));
+	if (z < -E_CONE_H + 0.1) { goto emid1; }
+	t1 = t2;
+}
+z = -z;
+if (z < 0.1) { goto emid1; }
+if (t1 < rayFirst) {goto emid1;}
+tmp = E_CONE_YR_P - (E_CONE_YR_Y * z); // tmp == r
+if (tmp <= 0.0) {goto emid1;}
+tmp = 1 / tmp;
+cot = t1;
+t2 = t1 - 0.00001;
+z = z0 + vz * t2;
+y = y0 + vy * t2;
+x = x0 + vx * t2;
+nz = E_CONE_DY;
+ny =  (y - 0.5) * tmp;
+nx = (x - mx - 0.5) * tmp;
+tResult = t1; doThing = 0; cox = x; coy = y; coz = z; conx= nx; cony = ny; conz = nz;
+emid1:
+if (vz == 0.0) {goto emid2;}
+t1 = (mz + 1.0 - z0) / vz;
+if (t1 < rayFirst) {goto emid2;}
+if (t1 > tResult) {goto emid2;}
+y = y0 + vy * t1 - 0.5;
+x = x0 + vx * t1 - mx - 0.5;
+t2 = (y * y) + (x * x);
+if (t2 > RAY_R_2) {goto emid2;}
+cot = t1;
+tResult = t1; doThing = 1;
+emid2:
+a = (vz * vz * 16.0) + (vy * vy) + (vx * vx);
+z = mz + 0.75 - z0;
+y = 0.5 - y0;
+x = mx + 0.5 - x0;
+k = (16.0 * z * vz) + (y * vy) + (x * vx);
+c = (16.0 * z * z) + (y * y) + (x * x) - 0.04;
+D = k * k - a * c;
+if (D < 0.0) {goto emid3;}
+D = sqrt(D);
+a = 1 / a;
+t1 = (k - D) * a;
+if (t1 < rayFirst) {t1 = (k + D) * a;}
+if (t1 < rayFirst) {goto emid3;}
+if (t1 > tResult) {goto emid3;}
+t1 = t1 - 0.0001;
+coz = z0 + vz * t1;
+coy = y0 + vy * t1;
+cox = x0 + vx * t1;
+conz = 5.0 * 4.0 * (coz - mz - 0.75);
+cony = 5.0 * (coy - 0.5);
+conx = 5.0 * (cox - 0.5 - mx);
+cot = t1;
+tResult = t1; doThing = 2;
+emid3:
+a = (vz * vz * 1.44) + (vy * vy) + (vx * vx);
+z = mz + 0.1 - z0;
+y = 0.5 - y0;
+x = mx + 0.5 - x0;
+k = (1.44 * z * vz) + (y * vy) + (x * vx);
+c = (1.44 * z * z) + (y * y) + (x * x) - 0.09;
+D = k * k - a * c;
+if (D < 0.0) {goto emid4;}
+D = sqrt(D);
+a = 1 / a;
+t1 = (k - D) * a;
+if (t1 < rayFirst) {t1 = (k + D) * a;}
+if (t1 < rayFirst) {goto emid4;}
+if (t1 > tResult) {goto emid4;}
+t1 = t1 - 0.0001;
+coz = z0 + vz * t1;
+if (coz < mz) { goto emid4;}
+coy = y0 + vy * t1;
+cox = x0 + vx * t1;
+conz = 3.333333333333333333 * 1.2 * (coz - mz - 0.1);
+cony = 3.333333333333333333 * (coy - 0.5);
+conx = 3.333333333333333333 * (cox - 0.5 - mx);
+cot = t1;
+tResult = t1; doThing = 3;
+emid4:
+if (vz == 0.0) {goto emi5;}
+t1 = (mz - z0) / vz;
+if (t1 + 0.00002 < rayFirst) {goto emi5;}
+if (t1 > tResult) {goto emi5;}
+y = y0 + vy * t1 - 0.5;
+x = x0 + vx * t1 - mx - 0.5;
+t2 = (y * y) + (x * x);
+if (t2 > CUT_EMI_R_2) {goto emi5;}
+t1 = t1 - 0.0001;
+coz = z0 + vz * t1;
+coy = y0 + vy * t1;
+cox = x0 + vx * t1;
+conz = -1.0;
+cony = 0.0;
+conx = 0.0;
+cot = t1;
+tResult = t1; doThing = 4;
+
 					}
 emi5:
 
